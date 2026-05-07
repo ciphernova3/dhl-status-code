d711bb12e40844fe6ad0b72cc3f4ac7ac457afff
ciphernova3 <sahanabhat4u@gmail.com>
2026-05-07 23:44:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Excellence, Simply Delivered", "From Wheels To Wings"];
+const statusMessages = ["Excellence, Simply Delivered", "From Wheels to Wings", "DM me !help];
 
 
 let currentIndex = 0;
